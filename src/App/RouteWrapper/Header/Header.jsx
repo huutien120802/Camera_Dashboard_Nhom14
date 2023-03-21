fe0f78540aa97a276d2371847dbc95079991a8b1
MinhLoc282 <minhloc2802@gmail.com>
2023-03-21 19:22:34 +0700

    add header

--- src/App/RouteWrapper/Header/Header.jsx
+++ src/App/RouteWrapper/Header/Header.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import HeaderIcon from 'assets/Icons/HeaderIcon.png';
+import Notification from 'assets/Icons/Notification.png';
+import Search from 'assets/Icons/Search.png';
+
+import styles from './index.module.css';
+
 function Header({ title }) {
   return (
-    <div>{title}</div>
+    <div>
+      <div className={styles.HeaderWrapper}>
+        <div className={styles.HeaderTitleContainer}>
+          <img src={HeaderIcon} alt={title} className={styles.HeaderIcon} />
+
+          <div className={styles.Title}>
+            {title}
+          </div>
+        </div>
+
+        <div className={styles.RightContainer}>
+          <img src={Search} alt="search" />
+
+          <img src={Notification} alt="notification" />
+
+          <div className={styles.Divider} />
+
+          <div className={styles.ProfileContainer}>
+            Jones Ferdinand
+
+            <div className={styles.Circle} />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
