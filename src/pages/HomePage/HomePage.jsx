ca06d939c6624eae10be971cc57535c744078fae
Lojc19 <89731207+Lojc19@users.noreply.github.com>
2023-03-27 20:39:32 +0700

    HomePage BaLoc

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
 
+import styles from './index.module.css';
+
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <div className={styles.container}>
+      <div className={styles.container_description}>
+        <div className={styles.description_item}>
+          <p className={styles.title}>
+            Tổng Camera
+          </p>
+          <p className={styles.number}>
+            60
+          </p>
+        </div>
+        <div className={styles.description_item}>
+          <p className={styles.title}>
+            Tổng số Profile
+          </p>
+          <p className={styles.number}>
+            64
+          </p>
+        </div>
+      </div>
+      <div className={styles.container_camera}>
+        <div className={styles.camera_item} />
+        <div className={styles.camera_item} />
+        <div className={styles.camera_item} />
+        <div className={styles.camera_item} />
+      </div>
+    </div>
   );
 }
 
