c6759a6cd557c6ae492ce3a74e8ab539589a8386
htien120802 <dhtien120802@gmail.com>
2023-05-26 06:53:11 +0700

    fix

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ import videoRouter from './routes/videos.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 27017;
 
 app.use(cors());
 app.use(bodyParser.json());
